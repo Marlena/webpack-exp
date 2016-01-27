8dc5d00194d1ae321108d2b1d485cf1dfcb1c370
Marlena Compton <marlena.compton@gmail.com>
2016-01-26 22:10:38 -0500

    refactor methods into an ES6 class and refactor jasmine functions to use fat arrow

--- spec/calculator_kata_spec.js
+++ spec/calculator_kata_spec.js
@@ -1,51 +1,9 @@
-//refactor: make a class
 //method change these methods to method chaining
 //figured out that I need to make a class so that I can reuse the
 //references for method chaining
 require('./spec_helper');
 
-const add = function(numString){
-
-  let sum;
-
-  const parsedString = removeNewLine(numString);
-  const numArray = parseIntArray(parsedString);
-
-  //destructuring
-  const [addend] = numArray;
-
-  if(numArray.length > 1){
-    //arrow syntax
-    sum = numArray.reduce((previous, current) => previous + current);
-    return sum;
-  }
-  return (addend > 0) && addend || 0;
-};
-
-const parseIntArray = function (numString){
-
-  if (numString === '') return [0];
-
-  const intArray = [];
-  const numStringArray = numString.split([',']);
-
-  //arrow syntax
-  numStringArray.forEach(num => intArray.push(parseInt(num)) );
-
-  return intArray;
-};
-
-const removeNewLine = function (str) {
-  let parsedStr = '';
-  const regex = (/\n/);
-
-  parsedStr = str.replace(regex, ',');
-
-  return parsedStr;
-};
-
-
-describe('The Calculator Kata', function(){
+describe('The Calculator Kata', () => {
   let subject;
   let Calculator = require ('../src/calculator');
 
@@ -54,57 +12,61 @@ describe('The Calculator Kata', function(){
   });
 
   describe('Calculator', () => {
-    it('adds', () => {
-      expect(subject.add('')).toEqual(0);
-    });
-  });
 
-  describe('add', function(){
+    describe('add', () => {
 
-    it('given no numbers returns 0', function(){
-      expect(add('')).toEqual(0);
-    });
-
-    it('given 1 number returns the sum', function(){
-      expect(add('1')).toEqual(1);
-    });
-
-    it('given 2 numbers returns the sum', function(){
-      expect(add('1,2')).toEqual(3);
-    });
-
-    it('given a set of numbers, returns the sum', function(){
-      expect(add('1,2,3,5,8')).toEqual(19);
-    });
-
-    it('given a newline in the set of numbers, returns the sum', function(){
-      //“1\n2,3”  (will equal 6)
-      expect(add("1\n2,3")).toEqual(6);
-    });
-
-    describe('parseIntArray', function(){
-      it('given a string of numbers, returns and array of ints', function(){
-        expect(parseIntArray('1,2')).toEqual([1, 2])
+      it('given no numbers returns 0', () => {
+        expect(subject.add('')).toEqual(0);
       });
 
-      it('given a string with one number, returns the number as an int', function(){
-        expect(parseIntArray('1')).toEqual([1]);
+      it('given 1 number returns the sum', () => {
+        expect(subject.add('1')).toEqual(1);
       });
 
-      it('given an empty string returns 0', function(){
-        expect(parseIntArray('')).toEqual([0]);
+      it('given 2 numbers returns the sum', () => {
+        expect(subject.add('1,2')).toEqual(3);
+      });
+
+      it('given a set of numbers, returns the sum', () => {
+        expect(subject.add('1,2,3,5,8')).toEqual(19);
+      });
+
+      it('given a newline in the set of numbers, returns the sum', () => {
+        //“1\n2,3”  (will equal 6)
+        expect(subject.add("1\n2,3")).toEqual(6);
       });
     });
 
-    describe('RemoveNewLines', function(){
-      it('given a string containing newlines returns the string without them', function(){
-        expect(removeNewLine("1\n2,3")).toEqual("1,2,3");
+      describe('parseIntArray', () => {
+        beforeEach(() => {
+          subject = new Calculator('0');
+        });
+
+        it('given a string of numbers, returns and array of ints', () => {
+          expect(subject.parseIntArray('1,2')).toEqual([1, 2])
+        });
+
+        it('given a string with one number, returns the number as an int', () => {
+          expect(subject.parseIntArray('1')).toEqual([1]);
+        });
+
+        it('given an empty string returns 0', () => {
+          expect(subject.parseIntArray('')).toEqual([0]);
+        });
       });
 
-      it('given a string without a newline, returns the string unmutated', function(){
-        expect(removeNewLine('1,2,3')).toEqual("1,2,3");
-      });
-    });
+      describe('RemoveNewLines', () => {
+        beforeEach(() => {
+          subject = new Calculator('0');
+        });
 
+        it('given a string containing newlines returns the string without them', () => {
+          expect(subject.removeNewLine("1\n2,3")).toEqual("1,2,3");
+        });
+
+        it('given a string without a newline, returns the string unmutated', () => {
+          expect(subject.removeNewLine('1,2,3')).toEqual("1,2,3");
+        });
+      });
   });
 });
--- src/calculator.js
+++ src/calculator.js
@@ -1,13 +1,46 @@
-
 class Calculator{
   constructor(data){
     this.data = data;
   }
 
-  add(){
-    return parseInt(this.data);
-  }
-};
+  add(numString){
 
+    let sum;
+
+    const parsedString = this.removeNewLine(numString);
+    const numArray = this.parseIntArray(parsedString);
+
+    //destructuring
+    const [addend] = numArray;
+
+    if(numArray.length > 1){
+      //arrow syntax
+      sum = numArray.reduce((previous, current) => previous + current);
+      return sum;
+    }
+    return (addend > 0) && addend || 0;
+  };
+
+  removeNewLine(str){
+    let parsedStr = '';
+    const regex = (/\n/);
+    parsedStr = str.replace(regex, ',');
+
+    return parsedStr;
+  }
+
+  parseIntArray(numString){
+
+    if (numString === '') return [0];
+
+    const intArray = [];
+    numStringArray = numString.split([',']);
+
+    //arrow syntax
+    numStringArray.forEach(num => intArray.push(parseInt(num)) );
+
+    return intArray;
+  }
+}
 
 export {Calculator as default};
