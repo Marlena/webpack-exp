ae4047353bfd5da08400f1307dd60feb57e5fb4e
Marlena Compton <marlena.compton@gmail.com>
2016-03-19 21:34:49 -0400

    collapse all the things

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,77 +1,13 @@
 
 let Scrabble = {
-  score(word){
+  scoreWord(word){
 
     if (word === null || word.length <= 0) return 0;
 
-    return this.groupAll(word);
+    return this.calculateScore(word);
   },
 
-  groupEight(word){
-
-    let score = 0;
-
-    Array.from(word).map((character)=>{
-      if (character.includes('z') || character.includes('q')){
-        return score += 10;
-      }
-    });
-
-    return score;
-  },
-
-  groupSix(word){
-    let score = 0;
-
-    Array.from(word).map((character)=>{
-      if (character.includes('k') || character.includes('K')){
-        return score += 5;
-      }
-    });
-
-    return score;
-  },
-
-  groupFive(word){
-    let score = 0;
-    const scoreMap = {4:'F, H, V, W, Y, f, h, v, w, y'};
-
-    let scoreKey = Object.keys(scoreMap)[0];
-
-
-    let scoreList =  eval(`scoreMap[\'${scoreKey}\']`) + '';
-
-    Array.from(word).map((character) =>{
-      if (scoreList.includes(character)){
-        return score += parseInt(scoreKey);
-      }
-
-    });
-
-    return score;
-  },
-
-  groupOne(word){
-    let score = 0;
-
-    const scoreMap = {1:'A, E, I, O, U, L, N, R, S, T, a, e, i, o, u, l, n, r, s, t'};
-
-    let scoreKey = Object.keys(scoreMap)[0];
-
-
-    let scoreList =  eval(`scoreMap[\'${scoreKey}\']`) + '';
-
-    Array.from(word).map((character) =>{
-      if (scoreList.includes(character)){
-        return score += parseInt(scoreKey);
-      }
-
-    });
-
-    return score;
-  },
-
-  groupAll(word){
+  calculateScore(word){
     let score = 0;
 
     const scoreMap = {
